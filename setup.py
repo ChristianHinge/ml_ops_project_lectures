5512867824da57b014419a8cebdcd07cbadbcae9
Christian Hinge <christian@hingepedersen.dk>
2021-06-15 14:07:43 +0200

    changed setup back

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='my_mnist',
-    packages=find_packages(where="src"),
-    package_dir={"":"src"},
+    packages=find_packages(),
     version='0.1.0',
     description='MNIST Classification project used in the ML-OPS course at DTU.',
     author='Christian Hinge',
